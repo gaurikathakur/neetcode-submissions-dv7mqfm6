eb27bde68d4a1b4d87a1c056619efe74a93c4e44
Gaurika Thakur <gaurikathakur37@gmail.com>
2026-08-11 16:37:02 +0530

    Add: is-palindrome - submission-3

---
+++ Data Structures & Algorithms/is-palindrome/submission-3.py
@@ -0,0 +1,6 @@
+from collections import Counter
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        cleaned="".join(char.lower()for char in s if char.isalnum())
+        return cleaned==cleaned[::-1]
+        
